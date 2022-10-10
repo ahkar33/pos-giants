40eda80c6d07b5740170563551a3e56a894cc297
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-10-11 01:57:57 +0630

    changed route and added secret properties

--- src/main/java/com/giants/pos/controller/HomeController.java
+++ src/main/java/com/giants/pos/controller/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class HomeController {
 
-    @GetMapping("/sign-in")
+    @GetMapping("/")
     public String sayHi() {
         return "/sign-in";
     }
