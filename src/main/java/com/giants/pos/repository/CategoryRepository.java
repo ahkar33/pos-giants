d69f96d1e03ab5e32b95ce1fadd9a4157431f3ca
Rimuru-Tempest-26 <pkh2662003@gmail.com>
2022-10-13 00:13:46 +0630

    category create and show

--- src/main/java/com/giants/pos/repository/CategoryRepository.java
+++ src/main/java/com/giants/pos/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.giants.pos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.giants.pos.datamodel.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    List<Category> findAllByOrderByIdDesc();
     
 }
