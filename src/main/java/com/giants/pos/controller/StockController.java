dc0fac4c1882aa87e3d244be1d0634d33f2450b0
Rimuru-Tempest-26 <pkh2662003@gmail.com>
2022-10-13 14:30:25 +0630

    stock ready

---
+++ src/main/java/com/giants/pos/controller/StockController.java
@@ -0,0 +1,10 @@
+package com.giants.pos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/stock")
+public class StockController {
+    
+}
