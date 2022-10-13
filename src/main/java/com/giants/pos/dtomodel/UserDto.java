c5a2bd9ee29a4d19f4b2c62f44c0e562023ac7f9
sawmon <sawmon71293@gmail.com>
2022-10-13 16:15:45 +0630

    added security config for salesperson and validation for registration failed to go to the post mapping

--- src/main/java/com/giants/pos/dtomodel/UserDto.java
+++ src/main/java/com/giants/pos/dtomodel/UserDto.java
@@ -3,6 +3,10 @@ package com.giants.pos.dtomodel;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import com.giants.pos.datamodel.Role;
 
 import lombok.Getter;
@@ -13,20 +17,19 @@ import lombok.Setter;
 public class UserDto {
 
     private Long id;    
-
-    // @NotEmpty(message = "Email cannot be empty")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
 
-    // @NotEmpty(message = "Name cannot be empty")
-    // @Size(min=5, max=250, message = "Minimum 5 characters and Maximum 250 characters")
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min=5, max=250, message = "Minimum 5 characters and Maximum 250 characters")
     private String name;
 
-    // @NotEmpty(message = "Password cannot be empty")
-    // @Min(value = 8, message = "Your password must be at least 8 characters")
+    @NotEmpty(message = "Password cannot be empty")
+    @Min(value = 8, message = "Your password must be at least 8 characters")
     private String password;
 
-    // @NotEmpty(message = "Confirm Password cannot be empty")
-    // @Min(value = 8, message = "Your password must be at least 8 characters")
+     @NotEmpty(message = "Confirm Password cannot be empty")
+     @Min(value = 8, message = "Your password must be at least 8 characters")
     private String confirmPassword;
 
     private Set<Role> roles = new HashSet<>(); 
